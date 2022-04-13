18235a31b587e453632292e06d40490ce304d99a
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-04-13 21:31:25 +0700

    [ADD]  executive management screen

---
+++ src/views/main/ExecutiveView.vue
@@ -0,0 +1,497 @@
+<template>
+  <div class="executive-screen">
+    <div class="header">
+      <div class="line" />
+      <div class="header-section grid">
+        <div class="text">
+          <div class="header-text">Overall Executives</div>
+          <div class="thin-content-text">
+            This screen has features to show, add, edit, and delete your
+            executives
+          </div>
+        </div>
+        <BaseButton
+          buttonType="common-button"
+          btnText="Add executive +"
+          textColor="#23106D"
+          textHover="white"
+          color="#DBD2FF"
+          hoverColor="#23106D"
+        >
+        </BaseButton>
+      </div>
+    </div>
+    <div class="body">
+      <div class="first-body-section grid">
+        <div class="filter-executive">
+          <div class="search-filter">
+            <div class="input-icon">
+              <i class="icon fa-solid fa-magnifying-glass"></i>
+              <input
+                type="text"
+                placeholder="Search by name"
+                v-model="searchInput"
+              />
+            </div>
+          </div>
+          <div
+            class="list-executive-card"
+            :style="
+              filterByName.length >= 4
+                ? { overflowY: 'scroll' }
+                : { overflow: 'hidden' }
+            "
+          >
+            <transition-group name="route">
+              <ExecutiveComp
+                v-for="(executive, index) in filterByName"
+                :key="index"
+                @selectExecutive="selectExecutive"
+                :executive="executive"
+                :selectedId="selectedId"
+              />
+              <div
+                v-if="filterByName.length == 0"
+                class="remark-text not-found"
+              >
+                Not found
+              </div>
+            </transition-group>
+          </div>
+        </div>
+        <transition-group name="route">
+          <div class="executive-card grid" v-if="selectedExecutive">
+            <div @click="toggleDropDown" class="icon-dropdown">
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+
+            <div
+              class="dropdown__content"
+              :class="`${isShowDropdown ? 'is-show' : ''}`"
+            >
+              <ul>
+                <li>
+                  <i class="icon fa-solid fa-pencil"></i>
+                  <div class="thin-content-text">Edit profile</div>
+                </li>
+                <li><div class="line" /></li>
+                <li class="red-color">
+                  <i class="icon fa-solid fa-user-minus"></i>
+                  <div class="thin-content-text">Delete executive</div>
+                </li>
+              </ul>
+            </div>
+            <div class="left-side">
+              <div class="profile-image">
+                <img
+                  src="../../assets/decorations/sample_profile.png"
+                  alt="sample profile illustration"
+                />
+              </div>
+              <div class="executive-profile">
+                <div class="name remark-text">
+                  {{ selectedExecutive.title }}.
+                  {{ selectedExecutive.firstname }}
+                  {{ selectedExecutive.lastname }}
+                </div>
+                <div class="position thin-content-text">
+                  {{ selectedExecutive.position }}
+                </div>
+              </div>
+            </div>
+            <div class="right-side">
+              <div class="content-text title">Official Information</div>
+              <div class="email">
+                <div class="label bold-content-text">
+                  Email
+                  <div @click="copyLink('email-value')">
+                    <i class="icon fa-regular fa-copy"></i>
+                  </div>
+                </div>
+                <div class="content-text" id="email-value">
+                  {{ selectedExecutive.email }}
+                </div>
+              </div>
+              <div class="phone">
+                <div class="label bold-content-text">
+                  Phone number
+                  <div @click="copyLink('phone-value')">
+                    <i class="icon fa-regular fa-copy"></i>
+                  </div>
+                </div>
+                <div class="content-text" id="phone-value">
+                  {{ selectedExecutive.tel }}
+                </div>
+              </div>
+              <div class="secretary">
+                <div class="label bold-content-text">
+                  Report to
+                  <div @click="copyLink('secretary-value')">
+                    <i class="icon fa-regular fa-copy"></i>
+                  </div>
+                </div>
+                <div class="content-text" id="secretary-value">
+                  {{ selectedExecutive.reportTo }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </transition-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseButton from "../../components/UI/BaseButton.vue";
+import ExecutiveComp from "../../components/meeting/ExecutiveComp.vue";
+export default {
+  components: { BaseButton, ExecutiveComp },
+  name: "ExecutiveView",
+  data() {
+    return {
+      searchInput: "",
+      selectedExecutive: null,
+      selectedId: null,
+      executives: [],
+      isShowDropdown: false,
+    };
+  },
+  computed: {
+    filterByName() {
+      return this.executives.filter((executive) => {
+        return (
+          executive.firstname
+            .toLowerCase()
+            .includes(this.searchInput.toLowerCase()) ||
+          executive.lastname
+            .toLowerCase()
+            .includes(this.searchInput.toLowerCase())
+        );
+      });
+    },
+  },
+  methods: {
+    selectExecutive(id) {
+      this.selectedExecutive = this.executives.find((executive) => {
+        this.selectedId = id;
+        return executive.id == id;
+      });
+    },
+    copyLink(value) {
+      let copyText = document.getElementById(value).innerHTML;
+      navigator.clipboard.writeText(copyText);
+    },
+    toggleDropDown() {
+      this.isShowDropdown = !this.isShowDropdown;
+    },
+  },
+  mounted() {
+    window.onscroll = () => {
+      this.isShowDropdown = false;
+    };
+    this.executives = [
+      {
+        id: 1,
+        title: "Mr",
+        firstname: "Similan",
+        lastname: "Klinsmith",
+        position: "Chief Executive",
+        email: "similan@mail.kmutt.ac.th",
+        tel: "081-000-0000",
+        reportTo: "Alexander Macedonia",
+        imageProfile: "",
+      },
+      {
+        id: 2,
+        title: "Ms",
+        firstname: "Praepanwa",
+        lastname: "Tedprasit",
+        position: "Chief Executive",
+        email: "praepanwa@mail.kmutt.ac.th",
+        tel: "081-000-0000",
+        reportTo: "Alexander Macedonia",
+        imageProfile: "",
+      },
+      {
+        id: 3,
+        title: "Ms",
+        firstname: "Noparat",
+        lastname: "Prasongdee",
+        position: "Chief Executive",
+        email: "noparat@mail.kmutt.ac.th",
+        tel: "081-000-0000",
+        reportTo: "Alexander Macedonia",
+        imageProfile: "",
+      },
+    ];
+    if (this.executives.length > 0) {
+      this.selectedExecutive = this.executives[0];
+      this.selectedId = this.executives[0].id;
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/colors/webColors.scss";
+.executive-screen {
+  .header {
+    width: 100%;
+    height: 15rem;
+    background-color: $white;
+    align-items: center;
+    justify-content: center;
+    .line {
+      width: 90%;
+      margin: 0 5%;
+      height: 0.1rem;
+      background-color: $grey;
+    }
+    .header-section {
+      height: 100%;
+      grid-template-columns: 2fr 0.5fr;
+      padding: 3rem 5.5rem;
+      align-items: center;
+      justify-content: center;
+      .text {
+        .header-text {
+          color: $darkViolet;
+          margin-bottom: 1rem;
+        }
+        .thin-content-text {
+          color: $highlightViolet;
+        }
+      }
+    }
+  }
+  .body {
+    padding: 3rem;
+    .first-body-section {
+      grid-template-columns: 1fr 2fr;
+      column-gap: 3rem;
+      .filter-executive {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        row-gap: 1rem;
+        position: relative;
+        .search-filter {
+          position: relative;
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-items: center;
+          .input-icon {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-items: center;
+            input[type="text"] {
+              padding: 1rem 1.4rem;
+              width: 100%;
+              height: 4rem;
+              border-radius: 0.5rem;
+              border: none;
+              background-color: $white;
+              font-family: "Poppins", sans-serif;
+            }
+            input[type="text"]:focus {
+              outline: none;
+              border: 0.1rem solid $primaryViolet;
+            }
+            input::placeholder {
+              font-size: 1.4rem;
+              color: $darkGrey;
+            }
+            .icon {
+              position: absolute;
+              right: 0;
+              font-size: 1.4rem;
+              margin-right: 1rem;
+              color: $darkGrey;
+            }
+          }
+        }
+        .list-executive-card::-webkit-scrollbar {
+          display: block !important;
+          -ms-overflow-style: auto !important;
+          scrollbar-width: auto !important;
+          background-color: transparent;
+          width: 1.4rem;
+        }
+        .list-executive-card::-webkit-scrollbar-track {
+          margin: 1rem;
+          border-radius: 0.5rem;
+        }
+        .list-executive-card::-webkit-scrollbar-thumb {
+          background-color: $grey;
+          border-radius: 0.5rem;
+          transition: all 0.2s ease-in-out;
+        }
+        .list-executive-card::-webkit-scrollbar-thumb:hover {
+          background-color: $darkGrey;
+        }
+        .list-executive-card {
+          width: 100%;
+          height: 64rem;
+          border-radius: 1.5rem;
+          background-color: $white;
+          .not-found {
+            padding: 1.8rem;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 80%;
+            text-align: center;
+            color: $darkGrey;
+          }
+        }
+      }
+      .executive-card {
+        width: 100%;
+        height: 100%;
+        border-radius: 2.5rem;
+        background-color: $white;
+        padding: 5.4rem 6.4rem;
+        grid-template-columns: 0.65fr 1.35fr;
+        position: relative;
+        .icon-dropdown {
+          position: absolute;
+          top: 5.4rem;
+          right: 6.4rem;
+          font-size: 2.2rem;
+          color: $darkViolet;
+          cursor: pointer;
+          transition: 0.2s all ease-in-out;
+        }
+        .icon-dropdown:hover {
+          color: $primaryViolet;
+        }
+        .dropdown__content {
+          box-shadow: 0px 0px 5px $fadedViolet;
+          z-index: -1;
+          display: flex;
+          text-align: center;
+          justify-content: center;
+          align-items: center;
+          margin-top: 6rem;
+          position: absolute;
+          opacity: 0;
+          background-color: $white;
+          overflow: hidden;
+          padding: 2.8rem 3.6rem;
+          right: 6.4rem;
+          transition: 0.3s all ease-in-out;
+          border-radius: 1.5rem;
+          cursor: auto;
+
+          &.is-show {
+            transform: translateY(2rem);
+            opacity: 1;
+            z-index: 1;
+            cursor: pointer;
+          }
+          ul {
+            width: 100%;
+            list-style: none;
+            li {
+              color: $darkViolet;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              transition: 0.3s all ease-in-out;
+              .icon {
+                font-size: 1.4rem;
+                margin-right: 1rem;
+                transition: 0.3s all ease-in-out;
+              }
+              .line {
+                width: 100% !important;
+                margin: 1.5rem 0;
+                height: 0.1rem;
+                background-color: $grey;
+              }
+            }
+            li:hover {
+              color: $primaryViolet;
+              .icon {
+                color: $primaryViolet !important;
+              }
+            }
+            li.red-color:hover {
+              color: $error;
+              .icon {
+                color: $error !important;
+              }
+            }
+          }
+        }
+        .left-side {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          text-align: center;
+          row-gap: 1.2rem;
+          .profile-image {
+            border-radius: 2rem;
+            width: 14rem;
+            height: 14rem;
+            background-color: $fadedViolet;
+            padding: 2.4rem;
+            img {
+              width: 100%;
+              height: 100%;
+            }
+          }
+          .executive-profile {
+            display: flex;
+            flex-direction: column;
+            .name {
+              color: $darkViolet;
+            }
+            .position {
+              color: $highlightViolet;
+            }
+          }
+        }
+        .right-side {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          row-gap: 3rem;
+          .title {
+            color: $darkGrey;
+          }
+          .email,
+          .phone,
+          .secretary {
+            display: flex;
+            flex-direction: column;
+            row-gap: 1.2rem;
+            .label {
+              display: flex;
+              align-items: center;
+              .icon {
+                color: $darkGrey;
+                font-size: 1.8rem;
+                margin-left: 0.5rem;
+                cursor: pointer;
+                transition: 0.2s all ease-in-out;
+              }
+              .icon:hover {
+                color: $highlightViolet;
+              }
+            }
+            .content-text {
+              color: $primaryViolet;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
