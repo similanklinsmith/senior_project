dab27a91ea04e05d3d13df7c477bc46524954f1a
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-04-16 14:17:02 +0700

    [ADD] add tailwind and calendar component

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import store from "./store";
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import LitepieDatepicker from 'litepie-datepicker';
 library.add(fas);
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import "../public/general.scss";
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount("#app");
+import './index.css'
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).use(LitepieDatepicker).mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  purge: [
+    path.resolve(__dirname, './node_modules/litepie-datepicker/**/*.js')
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'litepie-primary': colors.violet, // color system for light mode
+        'litepie-secondary': colors.black // color system for dark mode
+      }
+    }
+  },
+  variants: {
+    extend: {
+      cursor: ['disabled'],
+      textOpacity: ['disabled'],
+      textColor: ['disabled']
+    }
+  },
+  plugins: []
+};
