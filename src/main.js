fea6f478ab863a4243a0236d3cbf82e1a4292738
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-03 21:06:23 +0700

    [UPDATE] add axios

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import LitepieDatepicker from 'litepie-datepicker';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 library.add(fas);
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import * as LottiePlayer from "@lottiefiles/lottie-player";
 import "../public/general.scss";
 import './index.css'
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).use(LitepieDatepicker).use(LottiePlayer).mount("#app");
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(VueAxios, axios).use(router).use(LitepieDatepicker).use(LottiePlayer).mount("#app");
