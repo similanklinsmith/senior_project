e26bc4eb6f495d541a204f0404db2a08bc879152
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-03 21:07:05 +0700

    [ADD] GET_EXECUTIVES

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,31 @@
 import { createStore } from "vuex";
-
+import axios from "axios";
+const BASE_URL = "http://localhost:9000";
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    executiveURL: `${BASE_URL}/executives`,
+    executives: [],
+  },
+  getters: {
+    getExecutives(state) {
+      return state.executives;
+    },
+  },
+  mutations: {
+    SET_EXECUTIVES(state, executives) {
+      state.executives = executives;
+    },
+  },
+  actions: {
+    async getExecutives(context) {
+      try {
+        const data = await axios.get(this.state.executiveURL);
+        context.commit("SET_EXECUTIVES", data.data);
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      }
+    },
+  },
   modules: {},
 });
