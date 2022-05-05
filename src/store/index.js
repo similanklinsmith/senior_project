7e2ff070ecf56d1ec1d7f845674fc38f8f9296a0
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-05 21:32:52 +0700

    [UPDATE] loading status

--- src/store/index.js
+++ src/store/index.js
@@ -5,27 +5,37 @@ export default createStore({
   state: {
     executiveURL: `${BASE_URL}/executives`,
     executives: [],
-  },
-  getters: {
-    getExecutives(state) {
-      return state.executives;
-    },
+    loadingStatus: false
   },
   mutations: {
+    GET_LOADING_STATUS(state, loadingStatus) {
+      state.loadingStatus = loadingStatus;
+    },
     GET_EXECUTIVES(state, executives) {
       state.executives = executives;
     },
   },
   actions: {
     async getExecutives(context) {
+      context.commit("GET_LOADING_STATUS",true);
       try {
         const data = await axios.get(this.state.executiveURL);
-        console.log(data);
-        context.commit("GET_EXECUTIVES", data.data);
+        context.commit("GET_EXECUTIVES", data.data.data.sort((a, b) => (a.first_name > b.first_name ? 1 : -1)));
+        console.log(data.data);
+        context.commit("GET_LOADING_STATUS",false);
       } catch (error) {
         // alert(error);
+        context.commit("GET_LOADING_STATUS",false);
         console.log(error);
       }
+    },
+  },
+  getters: {
+    getterLoadingStatus(state) {
+      return state.loadingStatus;
+    },
+    getterExecutives(state) {
+      return state.executives;
     },
   },
   modules: {},
