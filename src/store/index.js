1ca2ef4fc212ab30999fcf3106246860213c496c
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-06 20:35:55 +0700

    [ADD] GET EXECUTIVE BY SECRETARY ID

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,10 @@ const BASE_URL = process.env.VUE_APP_API_PATH;
 export default createStore({
   state: {
     executiveURL: `${BASE_URL}/executives`,
+    myExecutiveURL: `${BASE_URL}/executive`,
     executives: [],
-    loadingStatus: false
+    myExecutives: [],
+    loadingStatus: false,
   },
   mutations: {
     GET_LOADING_STATUS(state, loadingStatus) {
@@ -14,18 +16,80 @@ export default createStore({
     GET_EXECUTIVES(state, executives) {
       state.executives = executives;
     },
+    GET_MY_EXECUTIVES(state, executives) {
+      state.myExecutives = executives;
+    },
+    ADD_EXECUTIVES(state, executives) {
+      state.myExecutives.push(executives);
+    },
+    DELETE_MY_EXECUTIVE(state, id) {
+      const index = state.myExecutives.findIndex(
+        (executive) => executive.id == id
+      );
+      if (index !== -1) {
+        state.myExecutives.splice(index, 1);
+      }
+    },
   },
   actions: {
     async getExecutives(context) {
-      context.commit("GET_LOADING_STATUS",true);
+      context.commit("GET_LOADING_STATUS", true);
       try {
         const data = await axios.get(this.state.executiveURL);
-        context.commit("GET_EXECUTIVES", data.data.data.sort((a, b) => (a.first_name > b.first_name ? 1 : -1)));
-        console.log(data.data);
-        context.commit("GET_LOADING_STATUS",false);
+        context.commit(
+          "GET_EXECUTIVES",
+          data.data.data.sort((a, b) => (a.first_name > b.first_name ? 1 : -1))
+        );
+        // console.log(data.data);
+        context.commit("GET_LOADING_STATUS", false);
       } catch (error) {
         // alert(error);
-        context.commit("GET_LOADING_STATUS",false);
+        context.commit("GET_LOADING_STATUS", false);
+        console.log(error);
+      }
+    },
+    async getMyExecutives(context, id) {
+      // GET Executives by Secretary ID
+      context.commit("GET_LOADING_STATUS", true);
+      try {
+        const data = await axios.get(this.state.myExecutiveURL+"/"+id);
+        context.commit(
+          "GET_MY_EXECUTIVES",
+          data.data.data.sort((a, b) => (a.first_name > b.first_name ? 1 : -1))
+        );
+        // console.log(data.data);
+        context.commit("GET_LOADING_STATUS", false);
+      } catch (error) {
+        // alert(error);
+        context.commit("GET_LOADING_STATUS", false);
+        console.log(error);
+      }
+    },
+    async addExecutive(context, payload) {
+      const jsonExecutive = JSON.stringify(payload.newExecutive);
+      const blob = new Blob([jsonExecutive], {
+        type: "application/json",
+      });
+      const formData = new FormData();
+      formData.append("newExecutive", blob);
+      try {
+        const response = await axios.post(this.state.executiveURL,{ body: formData });
+        if (response.status == 400) {
+          console.log("need to login");
+        }
+        context.commit("ADD_EXECUTIVES", response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async deleteExecutive(context, id) {
+      try {
+        const response = await axios.delete(this.state.myExecutiveURL+"/"+id);
+        if (response.status == 400) {
+          console.log("need to login");
+        }
+        context.commit("DELETE_MY_EXECUTIVE", id);
+      } catch (error) {
         console.log(error);
       }
     },
@@ -36,6 +100,9 @@ export default createStore({
     },
     getterExecutives(state) {
       return state.executives;
+    },
+    getterMyExecutives(state) {
+      return state.myExecutives;
     },
   },
   modules: {},
