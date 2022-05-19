e55c29098457e6913556cf89918739faa653fd19
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-19 14:54:37 +0700

    [ADD] filter date

--- src/views/main/meeting/SentView.vue
+++ src/views/main/meeting/SentView.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="sent-body-section">
     <div class="inbox">
-      <div class="search-filter">
-        <div class="input-icon">
-          <i class="icon fa-solid fa-magnifying-glass"></i>
-          <input
-            class="small-text"
-            type="text"
-            placeholder="Search by title"
-            v-model="searchInput"
-          />
+      <div class="filter">
+        <div class="search-filter">
+          <div class="input-icon">
+            <i class="icon fa-solid fa-magnifying-glass"></i>
+            <input
+              id="search-input"
+              class="small-text"
+              type="text"
+              placeholder="Search by title"
+              v-model="searchInput"
+              @focus="onFocus"
+              @blur="onBlur"
+            />
+          </div>
         </div>
+        <div class="filter-list" @click="toggleDropdown">
+          <i class="fa-solid fa-filter icon"></i>
+          <div class="alert" v-if="filterDate"></div>
+        </div>
+        <div
+          class="dropdown__content"
+          :class="`${isShowDropdown ? 'is-show' : ''}`"
+        >
+          <ul>
+            <li>
+              <div class="input">
+                <label for="due" class="bold-small-text">Due Date</label>
+                <litepie-datepicker
+                  id="due"
+                  as-single
+                  :formatter="formatter"
+                  v-model="withInDate"
+                  :style="{ fontSize: '12px !important', marginTop: '1rem' }"
+                />
+              </div>
+            </li>
+            <li>
+              <BaseButton
+                buttonType="common-button"
+                btnText="Search"
+                textColor="#23106D"
+                textHover="#23106D"
+                color="#DBD2FF"
+                hoverColor="#A58EFF"
+                width="100%"
+                @onClick="handleFilterDate"
+              >
+              </BaseButton>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="filter-show">
+        <div class="small-text">
+          <span v-if="filterDate">With in: {{ filterDate }}</span>
+        </div>
+        <div class="small-text">Results: {{ filterByTitle.length }}</div>
       </div>
       <div class="inbox-list">
         <transition-group name="route">
@@ -18,7 +65,7 @@
             v-for="inbox in filterByTitle"
             :key="inbox.id"
             :id="inbox.id"
-            :title="inbox.tittle"
+            :title="inbox.title"
             :time="inbox.create_at"
             :selectedId="selectedId"
             @selectInbox="selectInbox"
@@ -31,20 +78,27 @@
     </div>
     <transition name="route">
       <div class="inbox-detail" v-if="selectedInbox != null">
-        <div class="title remark-text">{{ selectedInbox.tittle }}</div>
+        <div class="title remark-text">{{ selectedInbox.title }}</div>
         <div class="sent-from smallest-text">
-          sent from Katherine Perish at
+          has already sent by you at
           {{ formatDateTime(selectedInbox.create_at) }}
         </div>
+        <div class="line"></div>
         <div class="content">
           <div class="attendees">
             <div class="bold-small-text">Attendees:</div>
             <div
               class="small-text attendee-list"
-              v-for="attendee in selectedInbox.attendees.slice(0, slice)"
+              v-for="(attendee, index) in selectedInbox.attendees.slice(
+                0,
+                slice
+              )"
               :key="attendee"
             >
-              {{ formatAttendee(attendee) }},
+              {{ formatAttendee(attendee)
+              }}<span v-if="index != selectedInbox.attendees.length - 1"
+                >,</span
+              >
             </div>
             <span
               class="see-more bold-small-text"
@@ -90,7 +144,13 @@
             </div>
           </div>
         </div>
-        <div class="button">
+        <div
+          class="button"
+          v-if="
+            new Date(selectedInbox.due_date_time).toLocaleDateString() >=
+            new Date().toLocaleDateString()
+          "
+        >
           <BaseButton
             buttonType="common-button"
             btnText="Delete Poll"
@@ -112,7 +172,7 @@
     >
       <template v-slot:popupContent>
         This Poll(<span :style="{ color: '#C4C4C4 !important' }">{{
-          selectedInbox.tittle
+          selectedInbox.title
         }}</span
         >) will be deleted immediately after confirming!
       </template>
@@ -145,14 +205,25 @@
 </template>
 
 <script>
+import LitepieDatepicker from "litepie-datepicker";
 import InboxComp from "../../../components/meeting/InboxComp.vue";
 import BaseButton from "../../../components/UI/BaseButton.vue";
 import BasePopup from "../../../components/UI/BasePopup.vue";
 import { mapGetters, mapActions } from "vuex";
+import { ref } from "vue";
 
 export default {
   name: "InboxView",
-  components: { InboxComp, BaseButton, BasePopup },
+  components: { InboxComp, BaseButton, BasePopup, LitepieDatepicker },
+  setup() {
+    const formatter = ref({
+      date: "YYYY-MM-DD",
+      month: "MMM",
+    });
+    return {
+      formatter,
+    };
+  },
   data() {
     return {
       searchInput: "",
@@ -161,24 +232,54 @@ export default {
       slice: 2,
       isShowMore: false,
       isShowPopup: false,
+      isShowDropdown: false,
+      withInDate: "",
+      filterDate: "",
     };
   },
   computed: {
-    ...mapGetters(["getterMyPolls"]),
+    ...mapGetters(["getterMyPolls", "getterExecutiveTitles"]),
     getPollsList() {
       return this.$store.getters.getterMyPolls;
     },
     filterByTitle() {
       return this.getPollsList.filter((toBeConfirmed) => {
-        return toBeConfirmed.tittle
-          .toLowerCase()
-          .includes(this.searchInput.toLowerCase());
+        if (this.filterDate) {
+          return (
+            toBeConfirmed.title
+              .toLowerCase()
+              .includes(this.searchInput.toLowerCase()) &&
+            new Date(toBeConfirmed.create_at).toLocaleDateString() ==
+              this.filterDate
+          );
+        } else {
+          return toBeConfirmed.title
+            .toLowerCase()
+            .includes(this.searchInput.toLowerCase());
+        }
       });
     },
   },
   methods: {
-    ...mapActions(["getMyPolls"]),
+    ...mapActions(["getMyPolls", "getExecutiveTitle"]),
+    onFocus() {
+      document.getElementById("search-input").placeholder = "Type to find...";
+    },
+    onBlur() {
+      document.getElementById("search-input").placeholder = "Search by title";
+    },
+    toggleDropdown() {
+      this.isShowDropdown = !this.isShowDropdown;
+    },
+    handleFilterDate() {
+      this.filterDate =
+        this.withInDate ?? new Date(this.withInDate).toLocaleDateString();
+      this.isShowDropdown = false;
+      this.selectedInbox = null;
+    },
     selectInbox(id) {
+      this.isShowMore = false;
+      this.slice = 2;
       this.selectedInbox = this.getPollsList.find((toBeConfirmed) => {
         this.selectedId = id;
         return toBeConfirmed.id == id;
@@ -193,17 +294,21 @@ export default {
     },
     formatDateTime(dateTime) {
       var createDate = new Date(dateTime);
-      return createDate.toLocaleString();
+      return createDate.toDateString() + " " + createDate.toLocaleTimeString();
+    },
+    formatTitle(str) {
+      return this.getterExecutiveTitles[str];
     },
     formatAttendee(attendee) {
-      return `${attendee.Executive.title_code} ${attendee.Executive.first_name} ${attendee.Executive.last_name}`;
+      return `${this.formatTitle(attendee.executive.title_code)} ${
+        attendee.executive.first_name
+      } ${attendee.executive.last_name}`;
     },
     showAllAttendee(attendees) {
       this.isShowMore = !this.isShowMore;
       this.isShowMore ? (this.slice = attendees.length) : (this.slice = 2);
     },
     deletePollAppointment(id) {
-      console.log(id);
       this.$store.dispatch("deletePollAppointment", id);
       this.isShowPopup = false;
       setTimeout(
@@ -217,12 +322,24 @@ export default {
   },
   created() {
     this.getMyPolls();
+    this.getExecutiveTitle();
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../../../assets/colors/webColors.scss";
+ul {
+  width: 100%;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+  li {
+    color: $darkViolet;
+    transition: 0.3s all ease-in-out;
+  }
+}
 .response {
   display: flex;
   flex-direction: column;
@@ -263,6 +380,80 @@ export default {
     display: flex;
     flex-direction: column;
     row-gap: 1rem;
+    position: relative;
+    .filter {
+      display: flex;
+      column-gap: 0.5rem;
+      .filter-list {
+        min-width: 4rem;
+        min-height: 4rem;
+        background-color: $primaryViolet;
+        border-radius: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        transition: 0.3s all ease-in-out;
+        .alert {
+          transform: translateX(0.4rem) translateY(-0.4rem);
+          top: 0%;
+          right: 0%;
+          min-width: 1rem;
+          min-height: 1rem;
+          position: absolute;
+          border-radius: 50%;
+          background-color: $yellow;
+          outline: 0.2rem solid $white;
+        }
+        .icon {
+          font-size: 1.4rem;
+          color: $white;
+        }
+        &:hover {
+          background-color: $darkViolet;
+        }
+      }
+    }
+    .dropdown__content {
+      box-shadow: 0px 0px 5px $fadedViolet;
+      z-index: -1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 3rem;
+      position: absolute;
+      right: 0%;
+      opacity: 0;
+      width: 100%;
+      background-color: $white;
+      padding: 2.8rem;
+      transition: 0.3s all ease-in-out;
+      border-radius: 1.5rem;
+      cursor: auto;
+      &.is-show {
+        transform: translateY(2rem);
+        opacity: 1;
+        z-index: 1;
+        cursor: pointer;
+      }
+      input[type="text"] {
+        padding: 1rem 1.4rem;
+        width: 100%;
+        height: 4rem;
+        border-radius: 0.5rem;
+        border: none;
+        background-color: $primaryGrey;
+        font-family: "Poppins", sans-serif;
+      }
+      input[type="text"]:focus {
+        outline: none;
+        border: 0.1rem solid $primaryViolet;
+      }
+      input::placeholder {
+        font-size: 1.4rem;
+        color: $darkGrey;
+      }
+    }
     .search-filter {
       position: relative;
       width: 100%;
@@ -300,6 +491,12 @@ export default {
         }
       }
     }
+    .filter-show {
+      display: flex;
+      justify-content: space-between;
+      color: $highlightViolet;
+      margin: 0.4rem 0;
+    }
     .inbox-list {
       height: 66rem;
       width: 100%;
@@ -327,6 +524,12 @@ export default {
     display: flex;
     flex-direction: column;
     overflow: scroll;
+    .line {
+      margin-top: 2.4rem;
+      width: 100%;
+      height: 0.1rem;
+      background-color: $grey;
+    }
     .sent-from {
       color: $darkGrey;
     }
