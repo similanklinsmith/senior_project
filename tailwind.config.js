e1d83123e305c4b519ccfe3d1c4f4ec91cb8cc74
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-24 20:24:38 +0700

    [DELETE] unused comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,6 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    // spacing: {
-    //   '1': '4px',
-    //   '2': '8px',
-    //   '3': '12px',
-    //   '4': '16px',
-    //   '5': '20px',
-    //   '6': '24px',
-    // },
     fontSize: {
       'xs': '8px',
     },
