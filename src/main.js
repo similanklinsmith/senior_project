1914fd967cf54ae43708164340a4418a0744a197
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-24 20:32:10 +0700

    [EDIT] comment unused import

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {fas} from '@fortawesome/free-solid-svg-icons'
+// import {library} from '@fortawesome/fontawesome-svg-core'
+// import {fas} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import LitepieDatepicker from 'litepie-datepicker';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-library.add(fas);
+// library.add(fas);
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import * as LottiePlayer from "@lottiefiles/lottie-player";
