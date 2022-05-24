5e9cdcaf66e79b161c24ed00ae3a0210465e9824
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-05-24 20:26:55 +0700

    [UPDATE] compressed styles

--- src/views/splash/SplashView.vue
+++ src/views/splash/SplashView.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="image">
         <img
-          src="../../assets/decorations/splash.png"
+          src="@/assets/decorations/splash.png"
           alt="splash illustrations"
         />
       </div>
@@ -39,41 +39,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/colors/webColors.scss";
-.pop-up {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  z-index: 12;
-  background-color: $darkViolet;
-  padding: 2.4rem 1.6rem;
-  color: $white;
-  .flex-col-center {
-    animation-name: floating;
-    -webkit-animation-name: floating;
-    animation-duration: 2s;
-    -webkit-animation-duration: 2s;
-    animation-iteration-count: infinite;
-    -webkit-animation-iteration-count: infinite;
-  }
-  .logo {
-    .primary-violet {
-      color: $primaryViolet;
-    }
-    .yellow {
-      color: $yellow;
-    }
-    .faded-violet {
-      color: $fadedViolet;
-    }
-  }
-  .image {
-    width: 15rem;
-    height: 15rem;
-    img {
-      width: 100%;
-      height: 100%;
-    }
-  }
-}
+@import "@/assets/colors/webColors.scss";
+.pop-up {width: 100%;height: 100%;position: fixed;z-index: 12;background-color: $darkViolet;padding: 2.4rem 1.6rem;color: $white;.flex-col-center {animation-name: floating;-webkit-animation-name: floating;animation-duration: 2s;-webkit-animation-duration: 2s;animation-iteration-count: infinite;-webkit-animation-iteration-count: infinite;}.logo {.primary-violet {color: $primaryViolet;}.yellow {color: $yellow;}.faded-violet {color: $fadedViolet;}}.image {width: 15rem;height: 15rem;img {width: 100%;height: 100%;}}}
 </style>
