2151c5595f6270f3013bb1184074fbb6396d27b8
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-08-23 17:13:40 +0700

    [DELETE] unused code

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -24,40 +24,16 @@ export const auth = {
       AuthService.logout();
       commit("logout");
     },
-    register({ commit }, user) {
-      return AuthService.register(user).then(
-        (response) => {
-          commit("registerSuccess");
-          return Promise.resolve(response.data);
-        },
-        (error) => {
-          commit("registerFailure");
-          return Promise.reject(error);
-        }
-      );
-    },
   },
   mutations:{
     loginSuccess(state, user) {
-        // let a = ((JSON.parse(localStorage.getItem("user")).token));
-        // const jwtDecodeValue = jwtDecrypt(a);
-        // console.log(jwtDecodeValue);
-        state.status.loggedIn = true;
         state.user = user;
     },
     loginFailure(state) {
-        state.status.loggedIn = false;
         state.user = null;
     },
     logout(state) {
-        state.status.loggedIn = false;
         state.user = null;
-    },
-    registerSuccess(state) {
-        state.status.loggedIn = false;
-    },
-    registerFailure(state) {
-        state.status.loggedIn = false;
     }
   }
 };
