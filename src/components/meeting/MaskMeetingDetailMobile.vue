d9b3b52e2a5c8963b7f2ce6e60fbdddbe9ddc8e1
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-09-02 16:44:52 +0700

    [FIX] height container

--- src/components/meeting/MaskMeetingDetailMobile.vue
+++ src/components/meeting/MaskMeetingDetailMobile.vue
@@ -50,5 +50,5 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/assets/colors/webColors.scss";
-.meeting-view-detail {background-color: $white;height: 100%;width: 100%;display: flex;flex-direction: column;padding: 1.6rem 0;.back-button {padding: 3.2rem;.icon {font-size: 2.8rem;}}.line {width: 100%;height: 0.1rem;background-color: $darkGrey;}.view-detail {display: flex;flex-direction: column;row-gap: 2rem;padding: 3.6rem 3.2rem;.title.remark-text {color: $darkViolet;line-height: 1.4;}.date-time {color: $darkGrey;font-size: 1.8rem;line-height: 1.6;}}}
+.meeting-view-detail {background-color: $white;height: 100vh;width: 100%;display: flex;flex-direction: column;padding: 1.6rem 0;.back-button {padding: 3.2rem;.icon {font-size: 2.8rem;}}.line {width: 100%;height: 0.1rem;background-color: $darkGrey;}.view-detail {display: flex;flex-direction: column;row-gap: 2rem;padding: 3.6rem 3.2rem;.title.remark-text {color: $darkViolet;line-height: 1.4;}.date-time {color: $darkGrey;font-size: 1.8rem;line-height: 1.6;}}}
 </style>
