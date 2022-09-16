817f118f93e309a894f776ab20dfbc0bb3164be9
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-09-16 16:36:02 +0700

    [ADD] auto fill endtime

--- src/components/meeting/ResponseComp.vue
+++ src/components/meeting/ResponseComp.vue
@@ -98,9 +98,7 @@
             />
           </div>
           <div class="input-form">
-            <label for="to" class="bold-small-text"
-              >To</label
-            >
+            <label for="to" class="bold-small-text">To</label>
             <input
               class="small-text readonly"
               type="time"
@@ -127,7 +125,10 @@
             ></BaseButton
           >
         </div>
-        <div class="error-container bold-content-text" v-if="errors.overlap || errors.duration">
+        <div
+          class="error-container bold-content-text"
+          v-if="errors.overlap || errors.duration"
+        >
           {{ errors.overlap }} {{ errors.duration }}
         </div>
         <div class="time-slots">
@@ -225,14 +226,14 @@ export default {
       return this.timeSlot.from < this.timeSlot.to;
     },
     durationTimeIsValid() {
-      var minDuration = this.duration*60;
+      var minDuration = this.duration * 60;
       var fromTime = this.timeSlot.from.split(/[- :]/);
       var toTime = this.timeSlot.to.split(/[- :]/);
       var fromDate = new Date();
       var toDate = new Date();
       fromDate.setHours(fromTime[0], fromTime[1], 0);
       toDate.setHours(toTime[0], toTime[1], 0);
-      return ((toDate.getTime() - fromDate.getTime())/60000) == minDuration;
+      return (toDate.getTime() - fromDate.getTime()) / 60000 == minDuration;
     },
     getOverlaps() {
       if (this.selectTimeSlots.length > 0) {
@@ -252,7 +253,6 @@ export default {
             (oCompareEvent.to <= oEvent.from && oCompareEvent.from > oEvent.to)
           ) {
             nOverlaps++;
-            console.log(nOverlaps);
             return false;
           }
         }
@@ -262,12 +262,35 @@ export default {
   },
   methods: {
     triggerFillToTime() {
-      console.log(this.timeSlot.from);
       var bits = this.timeSlot.from.split(/[- :]/);
       var date = new Date();
       date.setHours(bits[0], bits[1], 0);
-      var endTimeHour = date.getHours()+this.duration < 12 ? `${date.getHours()+this.duration}`.padStart(2, '0') : `${date.getHours()+this.duration}`;
-      var endTime = `${endTimeHour}:${date.getMinutes()}`;
+      var minuteDuration = this.duration * 60;
+      var endTimeHour;
+      var endTimeMinute;
+      var endTime;
+      if (minuteDuration % 60 != 0) {
+        endTimeMinute = ((date.getMinutes() + minuteDuration) % 60).toString().padStart(2, "0");
+        endTimeHour =
+          date.getHours() +
+            Math.floor((date.getMinutes() + minuteDuration) / 60) <
+          12
+            ? `${
+                date.getHours() +
+                Math.floor((date.getMinutes() + minuteDuration) / 60)
+              }`.padStart(2, "0")
+            : `${
+                date.getHours() +
+                Math.floor((date.getMinutes() + minuteDuration) / 60)
+              }`;
+      } else {
+        endTimeMinute = date.getMinutes().toString().padStart(2, "0");
+        endTimeHour =
+          date.getHours() + this.duration < 12
+            ? `${date.getHours() + this.duration}`.padStart(2, "0")
+            : `${date.getHours() + this.duration}`;
+      }
+      endTime = `${endTimeHour}:${endTimeMinute}`;
       this.timeSlot.to = endTime;
     },
     getDateObj(a) {
@@ -329,7 +352,7 @@ export default {
       this.confirmResponse();
       this.timeSlot.from = null;
       this.timeSlot.to = null;
-      this.errors = {}
+      this.errors = {};
     },
     selectInbox(id) {
       this.selectedInbox = this.toBeConfirmedList.find((toBeConfirmed) => {
@@ -354,7 +377,6 @@ export default {
           from: this.timeSlot.from,
           to: this.timeSlot.to,
         };
-        console.log(this.timeSlot.from > this.timeSlot.to);
         this.selectTimeSlots.push(selectedTime);
         // this.timeSlots.push(selectedTime);
         this.timeSlot.from = "";
@@ -370,213 +392,11 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/assets/colors/webColors.scss";
-.readonly {
-  background-color: $grey !important;
-}
-.modal {
-  width: 100%;
-  height: 100vh;
-  position: fixed;
-  background-color: rgba(24, 24, 26, 0.4);
-  z-index: 11;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-.pop-up {
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  position: fixed;
-  z-index: 12;
-  border-radius: 2.5rem;
-  display: flex;
-  flex-direction: column;
-  background-color: $white;
-  width: 80rem;
-  padding: 2.4rem;
-  animation-name: appears;
-  animation-duration: 0.5s;
-  animation-iteration-count: 1;
-  .required {
-    color: $error;
-    margin-left: 0.2rem;
-    font-size: 1.4rem !important;
-  }
-  .error-container {
-    width: 100%;
-    padding: 1rem 2rem;
-    border-radius: 1rem;
-    background-color: $error;
-    color: $white !important;
-    margin-bottom: 1.8rem;
-    text-align: center;
-  }
-  .confirm-button {
-    display: flex;
-    justify-content: flex-end;
-  }
-  .time-slots {
-    display: flex;
-    flex-direction: column;
-    row-gap: 1rem;
-    height: 24rem;
-    overflow: scroll;
-    margin-bottom: 1rem;
-    .time-slot {
-      width: 100%;
-      height: auto;
-      padding: 1.2rem 3.2rem;
-      background-color: $fadedViolet;
-      border-radius: 1rem;
-      .row {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .column {
-          display: flex;
-          flex-direction: column;
-          row-gap: 0.5rem;
-          .bold-small-text {
-            color: $darkViolet;
-          }
-          .small-text {
-            color: $white;
-          }
-        }
-      }
-    }
-  }
-  .title {
-    color: $primaryViolet;
-  }
-  .duration {
-    margin-top: 1rem;
-    color: $darkViolet;
-    .duration-label {
-      background-color: $yellow;
-      color: $fakeDark;
-      border-radius: 1rem;
-      padding: 0.5rem 1.75rem;
-      margin-left: 1rem;
-      font-weight: 600;
-    }
-  }
-  .form {
-    display: flex;
-    justify-content: space-around;
-    column-gap: 1rem;
-    align-items: flex-end;
-    margin: 2rem 0;
-    .input-form {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      .gray-format {
-        color: $darkGrey;
-        font-weight: 400;
-      }
-    }
-    select,
-    input {
-      margin-top: 1rem;
-      padding: 1rem 1.4rem;
-      width: 100%;
-      height: 4rem;
-      border-radius: 0.5rem;
-      border: none;
-      background-color: $primaryGrey;
-      font-family: "Poppins", sans-serif;
-    }
-    input:focus {
-      outline: none;
-      border: 0.1rem solid $primaryViolet;
-    }
-    select::placeholder,
-    input::placeholder {
-      font-size: 1.4rem;
-      color: $darkGrey;
-    }
-    select {
-      font-size: 1.4rem;
-    }
-    select:focus {
-      outline: none;
-      border: 0.1rem solid $primaryViolet;
-    }
-  }
-}
-.executive-response {
-  margin: 1rem 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  span.desktop {
-    color: $primaryViolet;
-  }
-  span.mobile {
-    display: none;
-    color: $primaryViolet;
-  }
-}
-.buttons {
-  display: flex;
-  column-gap: 0.5rem;
-}
-.add-time-slot {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  .label-text {
-    margin-bottom: 1rem;
-  }
-  .bold-smallest-text {
-    color: $darkGrey;
-  }
-  .show-time-slot {
-    width: fit-content;
-    border: 1px solid $darkViolet;
-    padding: 1.2rem 1.8rem;
-    border-radius: 0.5rem;
-    margin: 0.25rem 0;
-    .bold-smallest-text {
-      color: $darkViolet !important;
-    }
-  }
-}
-@media (max-width: 26.75em) {
-  .pop-up {
-    row-gap: 1rem;
-    width: 90%;
-    .form {
-      flex-wrap: wrap;
-      justify-content: flex-end;
-      row-gap: 1.6rem;
-    }
-  }
-  .executive-response {
-    .executive-name {
-      font-size: 2rem;
-    }
-    span.desktop {
-      display: none;
-    }
-    span.mobile {
-      display: inline;
-    }
-  }
-  .add-time-slot {
-    .bold-smallest-text {
-      font-size: 1.6rem;
-    }
-    .show-time-slot {
-      padding: 1.8rem 2.4rem;
-      margin: 0.75rem 0;
-      .bold-smallest-text {
-        font-size: 1.8rem;
-      }
-    }
-  }
-}
+.readonly {background-color: $grey !important;}
+.modal {width: 100%;height: 100vh;position: fixed;background-color: rgba(24, 24, 26, 0.4);z-index: 11;display: flex;flex-direction: column;align-items: center;justify-content: center;}
+.pop-up {top: 50%;left: 50%;transform: translate(-50%, -50%);position: fixed;z-index: 12;border-radius: 2.5rem;display: flex;flex-direction: column;background-color: $white;width: 80rem;padding: 2.4rem;animation-name: appears;animation-duration: 0.5s;animation-iteration-count: 1;.required {color: $error;margin-left: 0.2rem;font-size: 1.4rem !important;}.error-container {width: 100%;padding: 1rem 2rem;border-radius: 1rem;background-color: $error;color: $white !important;margin-bottom: 1.8rem;text-align: center;}.confirm-button {display: flex;justify-content: flex-end;}.time-slots {display: flex;flex-direction: column;row-gap: 1rem;height: 24rem;overflow: scroll;margin-bottom: 1rem;.time-slot {width: 100%;height: auto;padding: 1.2rem 3.2rem;background-color: $fadedViolet;border-radius: 1rem;.row {display: flex;justify-content: space-between;align-items: center;.column {display: flex;flex-direction: column;row-gap: 0.5rem;.bold-small-text {color: $darkViolet;}.small-text {color: $white;}}}}}.title {color: $primaryViolet;}.duration {margin-top: 1rem;color: $darkViolet;.duration-label {background-color: $yellow;color: $fakeDark;border-radius: 1rem;padding: 0.5rem 1.75rem;margin-left: 1rem;font-weight: 600;}}.form {display: flex;justify-content: space-around;column-gap: 1rem;align-items: flex-end;margin: 2rem 0;.input-form {width: 100%;display: flex;flex-direction: column;.gray-format {color: $darkGrey;font-weight: 400;}}select,input {margin-top: 1rem;padding: 1rem 1.4rem;width: 100%;height: 4rem;border-radius: 0.5rem;border: none;background-color: $primaryGrey;font-family: "Poppins", sans-serif;}input:focus {outline: none;border: 0.1rem solid $primaryViolet;}select::placeholder,input::placeholder {font-size: 1.4rem;color: $darkGrey;}select {font-size: 1.4rem;}select:focus {outline: none;border: 0.1rem solid $primaryViolet;}}}
+.executive-response {margin: 1rem 0;display: flex;justify-content: space-between;align-items: center;span.desktop {color: $primaryViolet;}span.mobile {display: none;color: $primaryViolet;}}
+.buttons {display: flex;column-gap: 0.5rem;}
+.add-time-slot {display: flex;flex-direction: column;justify-content: flex-start;.label-text {margin-bottom: 1rem;}.bold-smallest-text {color: $darkGrey;}.show-time-slot {width: fit-content;border: 1px solid $darkViolet;padding: 1.2rem 1.8rem;border-radius: 0.5rem;margin: 0.25rem 0;.bold-smallest-text {color: $darkViolet !important;}}}
+@media (max-width: 26.75em) {.pop-up {row-gap: 1rem;width: 90%;.form {flex-wrap: wrap;justify-content: flex-end;row-gap: 1.6rem;}}.executive-response {.executive-name {font-size: 2rem;}span.desktop {display: none;}span.mobile {display: inline;}}.add-time-slot {.bold-smallest-text {font-size: 1.6rem;}.show-time-slot {padding: 1.8rem 2.4rem;margin: 0.75rem 0;.bold-smallest-text {font-size: 1.8rem;}}}}
 </style>
