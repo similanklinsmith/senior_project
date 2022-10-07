869850d35d376b74e97a84fb645b540e9531d1b0
unknown <similan.klinsmith@mail.kmutt.ac.th>
2022-10-07 16:27:29 +0700

    [FIX] UI

--- src/components/UI/BaseNotFound.vue
+++ src/components/UI/BaseNotFound.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="remark-text flex-col-center loading-container">
     <div class="failed flex-col-center" v-if="isFailed">
-      <div class="header-fail">OOPS!</div>
       <div class="image">
         <img
           src="@/assets/decorations/not_found.png"
           alt="sending illustrations"
         />
       </div>
+      <div class="header-fail">OOPS!</div>
       We can't seem to find the page you're looking for.
-      <BaseButton
-        buttonType="common-button"
-        btnText="Go back"
-        textColor="white"
-        textHover="white"
-        color="#7452FF"
-        hoverColor="#23106D"
-        width="100%"
-        @onClick="$router.back()"
-      >
-      </BaseButton>
+        <BaseButton
+          buttonType="common-button"
+          btnText="Go back"
+          textColor="white"
+          textHover="white"
+          color="#7452FF"
+          hoverColor="#23106D"
+          width="100%"
+          @onClick="$router.back()"
+        />
     </div>
     <div class="flex-col-center loading" v-else>Loading...</div>
   </div>
@@ -37,6 +36,6 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/colors/webColors.scss";
 @media (max-width: 26.75em) {.loading-container {.failed {width: 60% !important;}}}
-.loading-container {height: 100vh;.failed {width: 30%;text-align: center;row-gap: 1rem;.header-fail {font-size: 4rem;font-weight: 600;color: $primaryViolet;}.image {width: 20rem;height: 20rem;img {width: 100%;height: 100%;object-fit: contain;}}}}
-.loading {height: 100vh;color: $highlightViolet;animation-name: floating;-webkit-animation-name: floating;animation-duration: 3s;-webkit-animation-duration: 3s;animation-iteration-count: infinite;-webkit-animation-iteration-count: infinite;}
+.loading-container {height: 100vh;.failed {width: 30%;text-align: center;row-gap:2rem;.header-fail {font-size: 4rem;font-weight: 600;color: $error;}.image {width: 20rem;height: 20rem;img {width: 100%;height: 100%;object-fit: contain;}}}}
+.loading,.image {height: 100vh;color: $highlightViolet;animation-name: floating;-webkit-animation-name: floating;animation-duration: 3s;-webkit-animation-duration: 3s;animation-iteration-count: infinite;-webkit-animation-iteration-count: infinite;}
 </style>
